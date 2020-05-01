341d542e4462163139c825dd58e05c241ec86105
Povilas Balciunas <balciunas90@gmail.com>
2020-05-01 22:29:32 +1200

    Implement CONNECT tunnel
    Seems to work now :)

--- src/io.rs
+++ src/io.rs
@@ -1,10 +1,20 @@
 //! I/O related code.
 
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::future::{Future};
+use std::pin::Pin;
+use std::task::{Poll, Context};
+use std::collections::VecDeque;
+
 use async_std::{io, task};
-use async_std::io::ReadExt;
+use async_std::io::{ReadExt, Read};
 use async_std::io::prelude::WriteExt;
 use async_std::net::{SocketAddr, IpAddr, Ipv4Addr, TcpListener, TcpStream};
 use async_std::stream::StreamExt;
+use httparse::Status;
+use unwrap::unwrap;
+use futures::pin_mut;
 
 use crate::proxy;
 
@@ -17,47 +27,160 @@ pub async fn run_server(port: u16) -> io::Result<()> {
 
     println!("Listening for connections on port {}", port);
 
-    while let Some(stream) = incoming.next().await {
-        let stream = stream?;
+    while let Some(Ok(stream)) = incoming.next().await {
         let _ = task::spawn(handle_connection(stream));
     }
 
     Ok(())
 }
 
-pub async fn handle_connection(mut stream: TcpStream) -> io::Result<()> {
-    println!("New incoming connection: {}", stream.peer_addr().unwrap());
-    let mut proxy_sess = proxy::Session::waiting_request();
-    let mut buf = vec![0u8; 4096];
+struct ProxyData {
+    stream1: TcpStream,
+    stream2: TcpStream,
+    stream1_in_buff: VecDeque<Vec<u8>>,
+    stream2_in_buff: VecDeque<Vec<u8>>,
+}
 
-    loop {
-        let bytes_read = stream.read(&mut buf).await?;
-        if bytes_read == 0 {
-            break;
-        }
-
-        proxy_sess = proxy_sess.on_data(&buf[..bytes_read]);
-        println!("{:?}", proxy_sess);
-        proxy_sess = match proxy_sess {
-            proxy::Session::OnConnectRequest(state) => {
-                let mut out_stream = TcpStream::connect(&state.path).await?;
-                // TODO(povilas): get this from ConnectTunnel state.
-                stream.write_all(b"HTTP/1.1 200 Connection established\r\n\r\n").await?;
-                proxy::Session::ConnectTunnel(state.on_connected(out_stream))
-            }
-            proxy::Session::ConnectTunnel(mut state) => {
-                state.conn.write_all(&buf[..bytes_read]).await?;
-                let bytes_read = state.conn.read(&mut buf).await?;
-                stream.write_all(&buf[..bytes_read]).await?;
-                proxy::Session::ConnectTunnel(state)
-            }
-            proxy::Session::Failed(_state) => {
-                println!("State machine failed");
-                return Ok(());
-            }
-            proxy_sess => proxy_sess,
+impl ProxyData {
+    fn new(stream1: TcpStream, stream2: TcpStream) -> Self {
+        Self {
+            stream1,
+            stream2,
+            stream1_in_buff: Default::default(),
+            stream2_in_buff: Default::default()
         }
     }
+}
+
+impl Future for ProxyData {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        println!("proxy_data.poll()");
+        let mut buff = [0u8; 65535];
+
+        // stream1 --> stream2
+        loop {
+            let fut = self.stream1.read(&mut buff);
+            pin_mut!(fut);
+
+            match fut.poll(cx) {
+                Poll::Pending => break,
+                Poll::Ready(Ok(bytes_read)) => {
+                    println!("stream1 bytes read: {}", bytes_read);
+                    if bytes_read == 0 {
+                        return Poll::Ready(());
+                    }
+
+                    self.stream1_in_buff.push_back(buff[..bytes_read].to_vec())
+                },
+                Poll::Ready(Err(e)) => {
+                    // TODO(povilas): return error
+                    println!("Failed to read from stream2: {}", e);
+                    return Poll::Ready(());
+                }
+            }
+        }
+
+        while let Some(buff) = self.stream1_in_buff.pop_front() {
+            let mut fut = self.stream2.write(&buff);
+            pin_mut!(fut);
+            // TODO(povilas): handle error
+            if let Poll::Ready(Ok(bytes_written)) = fut.poll(cx) {
+                if bytes_written < buff.len() {
+                    self.stream1_in_buff.push_front(buff[bytes_written..].to_vec());
+                }
+            } else {
+                break;
+            }
+        }
+
+        // stream2 --> stream1
+        loop {
+            let fut = self.stream2.read(&mut buff);
+            pin_mut!(fut);
+
+            match fut.poll(cx) {
+                Poll::Pending => break,
+                Poll::Ready(Ok(bytes_read)) => {
+                    println!("stream2 bytes read: {}", bytes_read);
+                    if bytes_read == 0 {
+                        return Poll::Ready(());
+                    }
+
+                    self.stream2_in_buff.push_back(buff[..bytes_read].to_vec())
+                },
+                Poll::Ready(Err(e)) => {
+                    // TODO(povilas): return error
+                    println!("Failed to read from stream2: {}", e);
+                    return Poll::Ready(());
+                }
+            }
+        }
+
+        while let Some(buff) = self.stream2_in_buff.pop_front() {
+            let mut fut = self.stream1.write(&buff);
+            pin_mut!(fut);
+
+            // TODO(povilas): handle error
+            if let Poll::Ready(Ok(bytes_written)) = fut.poll(cx) {
+                if bytes_written < buff.len() {
+                    self.stream2_in_buff.push_front(buff[bytes_written..].to_vec());
+                }
+            } else {
+                break;
+            }
+        }
+
+        Poll::Pending
+    }
+}
+
+pub async fn handle_connection(mut client_conn: TcpStream) -> io::Result<()> {
+    println!("New incoming connection: {}", client_conn.peer_addr().unwrap());
+
+    let mut buf = vec![0u8; 65535];
+
+    let bytes_read = client_conn.read(&mut buf).await?;
+    if bytes_read == 0 {
+        // TODO(povilas): return error instead with conn reset or smth
+        return Ok(());
+    }
+
+    // TODO(povilas): return error if not connect
+    let connect_to = parse_conn_request(&buf[..bytes_read]);
+    println!("Connecting to: {}", connect_to);
+
+    let target_conn = TcpStream::connect(connect_to).await?;
+    println!("...connected");
+
+    client_conn.write_all(b"HTTP/1.1 200 Connection established\r\n\r\n").await?;
+
+    ProxyData::new(client_conn, target_conn).await;
 
     Ok(())
+}
+
+// TODO(povilas): return result
+fn parse_conn_request(data: &[u8]) -> String {
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+
+    // TODO(povilas): return error
+    match unwrap!(req.parse(data)) {
+        Status::Complete(_bytes_parsed) => {
+            if let (Some(method), Some(path)) = (req.method, req.path) {
+                if method == "CONNECT" {
+                    return path.to_string()
+                } else {
+                    panic!("unsupported method: {}", method);
+                }
+            } else {
+                panic!("Couldn't parse method and path");
+            }
+        }
+        Status::Partial => {
+            panic!("HTTP request was partially parsed - not yet supported!");
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,6 @@ use httparse;
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
-    /*
-    let buf = b"CONNECT httpbin.org:443 HTTP/1.1\r\nHost: httpbin.org:443\r\n\r\nTLS handshake".to_vec();
-    let s = s.on_data(proxy::Data(buf));
-    let s = s.on_connected(proxy::Connected);
-    println!("{:?}", s);
-    */
     io::run_server(8080).await?;
     Ok(())
 }
